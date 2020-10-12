704e8394f5e851c5560afd436d67590c605b717d
Dmitry Veretelnikov <veretdmitry2000@mail.ru>
2020-10-12 22:05:54 +0300

    create: structure, header, tabs

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  var tabsItem = $('.tabs-cards__item');
+  var tabsContent = $('.tabs-content__item');
+  tabsItem.on('click', function(event) {
+    var activeContent = $(this).attr('data-target');
+    event.preventDefault();
+    $(this).removeAttr('href');
+    tabsContent.removeClass('tabs-content__item--active');
+    tabsItem.removeClass('tabs-cards__item--active');
+    $(this).addClass('tabs-cards__item--active');
+    $(activeContent).addClass('tabs-content__item--active');
+    $(activeContent).find('.tabs-content__theme').text($(this).find('.tabs-cards__title').text());
+    $(activeContent).find('.tabs-content__title').text($(this).find('.tabs-cards__text').text());
+    $(activeContent).find('.tabs-content__theme').css('color', $(this).find('.tabs-cards__title').css('color'));
+    $(activeContent).find('.tabs-button').css('background-color', $(this).find('.tabs-cards__title').css('color'));
+  });
+
+});
